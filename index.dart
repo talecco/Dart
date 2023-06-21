97adf1279df048ebdfe72292e0806ab73a15c10b
talecco <othalesborges@gmail.com>
2023-06-20 23:17:07 -0300

    Usando o Late

--- index.dart
+++ index.dart
@@ -1,6 +1,16 @@
+late double valorTotal;  
+
+//use late para identificar fora do main
+
 void main() {
     //var nomeCompleto = 'Thales Borges';
     int nomeCompleto = 9;
 
     print(nomeCompleto);
+
+    valorTotal = 1000; 
+
+  print(valorTotal);
 }
+
+
